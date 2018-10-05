eb7f47c3218e830cd4715d9ddbb682d30efa219a
ARTICUS939 <articus_mib@mail.ru>
2018-10-05 17:43:57 +0300

    практика 1

--- PractiseForEx1/Program.cs
+++ PractiseForEx1/Program.cs
@@ -10,6 +10,17 @@ namespace PractiseForEx1
     {
         static void Main(string[] args)
         {
+
+                for (int i = 1; i <= 10; i++)
+
+                if (i % 2 == 0)
+                {
+                    Convert.ToString(i);
+                    Console.Write(i + " ");    //выпишем все четные диапазона
+                }
+
+            Console.ReadLine();
+
         }
     }
 }
